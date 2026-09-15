d71ce4f417b535d3426aa7ccfd4d59ae05fe2cfc
Corporate Gadfly <haroon.rafique@gmail.com>
2026-09-15 18:24:02 -0400

    replace deprecated calls with Facter::Core::Execution
    Signed-off-by: Corporate Gadfly <haroon.rafique@gmail.com>

--- lib/facter/falco.rb
+++ lib/facter/falco.rb
@@ -2,10 +2,10 @@
 
 Facter.add(:falco_driver_version) do
   confine kernel: :Linux
-  confine { Facter::Util::Resolution.which('falco') }
+  confine { Facter::Core::Execution.which('falco') }
 
   setcode do
-    falco_driver_version = Facter::Util::Resolution.exec('falco --version')
+    falco_driver_version = Facter::Core::Execution.execute('falco --version')
     falco_driver_version.match(%r{\d+\.\d+\.\d+\+driver}).to_s
   end
 end
